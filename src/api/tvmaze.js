bc11cf0ef22407e312fe4b3bdf1d1c43d9469374
shelooks16 <blauyedz@zoho.eu>
2022-11-13 16:59:47 +0000

    added search for actors

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -8,3 +8,5 @@ const apiGet = async queryString => {
 };
 
 export const searchForShows = query => apiGet(`/search/shows?q=${query}`);
+
+export const searchForPeople = query => apiGet(`/search/people?q=${query}`);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react';
-import { searchForShows } from './../api/tvmaze';
+import { searchForShows, searchForPeople } from './../api/tvmaze';
 
 const Home = () => {
   const [searchStr, setSearchStr] = useState('');
   const [apiData, setApiData] = useState(null);
   const [apiDataError, setApiDataError] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
 
   const onSearchInputChange = ev => {
     setSearchStr(ev.target.value);
+  };
+
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
   };
 
   const onSearch = async ev => {
@@ -16,8 +21,13 @@ const Home = () => {
     try {
       setApiDataError(null);
 
-      const result = await searchForShows(searchStr);
-      setApiData(result);
+      if (searchOption === 'shows') {
+        const result = await searchForShows(searchStr);
+        setApiData(result);
+      } else {
+        const result = await searchForPeople(searchStr);
+        setApiData(result);
+      }
     } catch (error) {
       setApiDataError(error);
     }
@@ -29,9 +39,11 @@ const Home = () => {
     }
 
     if (apiData) {
-      return apiData.map(data => (
-        <div key={data.show.id}>{data.show.name}</div>
-      ));
+      return apiData[0].show
+        ? apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
+        : apiData.map(data => (
+            <div key={data.person.id}>{data.person.name}</div>
+          ));
     }
 
     return null;
@@ -41,6 +53,29 @@ const Home = () => {
     <div>
       <form onSubmit={onSearch}>
         <input type="text" value={searchStr} onChange={onSearchInputChange} />
+
+        <label>
+          Shows
+          <input
+            type="radio"
+            name="search-option"
+            value="shows"
+            checked={searchOption === 'shows'}
+            onChange={onRadioChange}
+          />
+        </label>
+
+        <label>
+          Actors
+          <input
+            type="radio"
+            name="search-option"
+            value="actors"
+            checked={searchOption === 'actors'}
+            onChange={onRadioChange}
+          />
+        </label>
+
         <button type="submit">Search</button>
       </form>
 
